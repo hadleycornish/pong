cd22d4a9329921ebe6105e385040fd7a2f04c18a
hadleycornish <hadleycornish@gmail.com>
2023-12-21 15:06:59 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,10 +49,10 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("paddle.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("silence.mp3");
+export var scoreSound = new soundFile("pongmusic.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("ambient.mp3");
+export var ambientSound = new soundFile("pongmusic.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
@@ -62,7 +62,7 @@ soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
 
-export var villageMusic = new soundFile("pong_music.mp3");
+export var villageMusic = new soundFile("pongmusic.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
 villageMusic.player.volume.value = -16;
